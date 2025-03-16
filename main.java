6314d827a3dac623abb1952bc424471b5add80e2
Liza <kalanchuk.liza@mail.ru>
2025-03-16 16:15:02 +0300

    Удалена папка src/

--- main.java
+++ main.java
@@ -1,16 +1,4 @@
-public class Main {
-    public static void main(String[] args) {
-        // Переменная стоимости билета
-        int priceOfTicket = 13_676;
+package PACKAGE_NAME;
 
-        // Переменная количества рублей одной бонусной мили
-        int rublesBonus = 20;
-
-        //Расчет количества бонусных миль
-        int bonusMiles = priceOfTicket / rublesBonus;
-
-        // Результат
-        System.out.println("Количество начисленных миль: " + bonusMiles);
-
-    }
+public class main {
 }
